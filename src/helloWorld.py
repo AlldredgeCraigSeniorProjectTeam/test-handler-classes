c1a76a78701cb388f892fc79e8beb198aa4c1e39
danielmartincraig <danielmartincraig@gmail.com>
2018-09-17 13:08:45 -0600

    Finished tutorial

--- src/helloWorld.py
+++ src/helloWorld.py
@@ -1,9 +1,15 @@
+import logging
+
 from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model import Response
 
 sb = SkillBuilder()
-
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_model.ui import SimpleCard
 
 class LaunchRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
@@ -13,5 +19,85 @@ class LaunchRequestHandler(AbstractRequestHandler):
         speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
 
         handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(False)
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            False)
+        return handler_input.response_builder.response
+
+
+class HelloWorldIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return (handler_input.request_envelope.request.object_type == "IntentRequest"
+            and handler_input.request_envelope.request.intent.name == "HelloWorldIntent")
+
+    def handle(self, handler_input):
+        speech_text = "Hello World"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text))
+
+        return handler_input.response_builder.response
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return (handler_input.request_envelope.request.object_type == "IntentRequest"
+            and handler_input.request_envelope.request.intent.name == "AMAZON.HelpIntent")
+
+    def handle(self, handler_input):
+        speech_text = "You can say hello to me!"
+
+        handler_input.response_builder.speak(speech_text).ask(speech_text).set_card(
+            SimpleCard("Hello World", speech_text))
+        
         return handler_input.response_builder.response        
+
+
+class CancelAndStopIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return (handler_input.request_envelope.request.object_type == "IntentRequest"
+            and (handler_input.request_envelope.request.intent.name == "AMAZON.CancelIntent"
+                or handler_input.request_envelope.request.intent.name == "AMAZON.StopIntent"))
+
+    def handle(self, handler_input):
+        speech_text = "Goodbye!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text))
+        return handler_input.response_builder.response
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("SessionEndedRequest")(handler_input)     
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        # any cleanup logic goes here
+
+        return handler_input.response_builder.response
+
+class AllExceptionHandler(AbstractExceptionHandler):
+    
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        # Log the exception in CloudWatch Logs
+        print(exception)
+
+        speech = "Sorry, there was some problem.  Please try again!!"
+        handler_input.response_builder.speak(speech).ask(speech)
+        
+        return handler_input.response_builder.response
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(HelloWorldIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelAndStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+
+sb.add_exception_handler(AllExceptionHandler())
+
+handler = sb.lambda_handler()
